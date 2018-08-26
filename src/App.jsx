65bba6865818a9390b8988019bf68e4696b3b7e7
segers <jo.segers@gmail.com>
2018-08-26 18:07:21 +0200

    Revert "Include bootstrap as frontend framework"
    This reverts commit a8b3e419947f833328c8e764982a3ea4adb51264.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React, { Component} from 'react';
-import { hot } from 'react-hot-loader';
-import PageTitle from './components/PageTitle'
+import React, { Component} from "react";
+import {hot} from "react-hot-loader";
+import "./App.css";
 
 class App extends Component{
   render(){
     return(
-      <div>
-        <PageTitle />
-        <p>Bio</p>
+      <div className="App">
+        <h1> Hello, World! </h1>
+        <p>This is a placeholder for my homepage. More to come soon...</p>
       </div>
     );
   }
--- src/components/PageTitle.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { PageHeader } from 'react-bootstrap';
-
-export default class PageTitle extends React.Component {
-  render() {
-    return <PageHeader>Jo Segers <small>information</small></PageHeader>;
-  };
-}
